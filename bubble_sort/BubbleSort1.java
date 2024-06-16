bc14e6b3be15523f729bece7e83ce54d1abc9272
arpit <arpit@endpointech.com>
2024-06-16 10:46:42 +0530

    BubbleSort program.

---
+++ bubble_sort/BubbleSort1.java
@@ -0,0 +1,31 @@
+package bubble_sort;
+
+public class BubbleSort1 {
+
+    public static void printSortedArray(int sortedNumbers[]) {
+        System.out.println("After sorting");
+        for (int i = 0; i < sortedNumbers.length - 1; i++) {
+            System.out.print(" " + sortedNumbers[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {2, 1, 4, 6, 3, 5, 6};
+        System.out.println("Before Sorting");
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            System.out.print(" " + numbers[i]);
+        }
+        System.out.println();
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - i - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+        printSortedArray(numbers);
+    }
+}
